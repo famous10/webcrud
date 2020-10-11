6cb8d0113b94bfd8d8a2111cacf7b71686d0804a
famous <famous0814@gmail.com>
2020-10-11 04:46:47 -0700

    first commit

---
+++ Edit.aspx
@@ -0,0 +1,106 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Edit.aspx.cs" Inherits="WebInserteEdit.Edit" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+           <div>
+        <asp:GridView ID="gvUserRegistration" runat="server" AutoGenerateColumns="false" ShowFooter="true" DataKeyNames="EditDb"
+             showHeaderWhenEmpty="true"
+            OnRowCommand="gvUserRegistration_RowCommand" OnRowEditing="gvUserRegistration_RowEditing" OnRowCancelingEdit="gvUserRegistration_RowCancelingEdit"
+             OnRowUpdating="gvUserRegistration_RowUpdating" OnRowDeleting="gvUserRegistration_RowDeleting"
+            BackColor="White" BorderColor="#CCCCCC" BorderStyle="None" BorderWidth="1px" CellPadding="3">
+
+            <FooterStyle BackColor="White" ForeColor="#000066" />
+            <HeaderStyle BackColor="#006699" Font-Bold="True" ForeColor="White" />
+            <PagerStyle BackColor="White" ForeColor="#000066" HorizontalAlign="Left" />
+            <RowStyle ForeColor="#000066" />
+            <SelectedRowStyle BackColor="#669999" Font-Bold="True" ForeColor="White" />
+            <SortedAscendingCellStyle BackColor="#F1F1F1" />
+            <SortedAscendingHeaderStyle BackColor="#007DBB" />
+            <SortedDescendingCellStyle BackColor="#CAC9C9" />
+            <SortedDescendingHeaderStyle BackColor="#00547E" />
+        
+ 
+      <Columns>
+                <asp:TemplateField HeaderText="firstname" >
+                    <ItemTemplate>
+                        <asp:TextBox  Text='<%#Eval("firstname")%>' runat="server" />
+                    </ItemTemplate>
+                    <EditItemTemplate>
+                         <asp:TextBox ID="txtfirstname" Text='<%#Eval("firstname")%>' runat="server" />
+                    </EditItemTemplate>
+                    <FooterTemplate>
+                              <asp:TextBox ID="txtfirstnamefooter"  runat="server" />
+                    </FooterTemplate>
+                </asp:TemplateField>
+                                <asp:TemplateField HeaderText="lastname">
+                    <ItemTemplate>
+                        <asp:TextBox  Text='<%#Eval("lastname")%>' runat="server" />
+                    </ItemTemplate>
+                    <EditItemTemplate>
+                         <asp:TextBox ID="txtlastname" Text='<%#Eval("lastname")%>' runat="server" />
+                    </EditItemTemplate>
+                    <FooterTemplate>
+                              <asp:TextBox ID="txtlastnamefooter"  runat="server" />
+                    </FooterTemplate>
+                </asp:TemplateField>
+                                <asp:TemplateField HeaderText="contact">
+                    <ItemTemplate>
+                        <asp:TextBox  Text='<%#Eval("contact")%>' runat="server" />
+                    </ItemTemplate>
+                    <EditItemTemplate>
+                         <asp:TextBox ID="txtcontact" Text='<%#Eval("contact")%>' runat="server" />
+                    </EditItemTemplate>
+                    <FooterTemplate>
+                              <asp:TextBox ID="txtcontactfooter"  runat="server" />
+                    </FooterTemplate>
+                </asp:TemplateField>
+                                <asp:TemplateField HeaderText="Email">
+                    <ItemTemplate>
+                        <asp:TextBox  Text='<%#Eval("Email")%>' runat="server" />
+                    </ItemTemplate>
+                    <EditItemTemplate>
+                         <asp:TextBox ID="txtEmail" Text='<%#Eval("Email")%>' runat="server" />
+                    </EditItemTemplate>
+                    <FooterTemplate>
+                              <asp:TextBox ID="txtEmailfooter"  runat="server" />
+                    </FooterTemplate>
+                </asp:TemplateField>
+                <asp:TemplateField>
+
+                    <ItemTemplate>
+
+                   <asp:ImageButton imageurl="images/icons8-edit-image-64.png" runat="server" commandName="edit" ToolTip="Edit" width="20px" Height="20px" />
+                   <asp:ImageButton imageurl="images/delete.png" runat="server" commandName="delete" ToolTip="delete" width="20px" Height="20px" />
+
+
+                    </ItemTemplate>
+                    <EditItemTemplate>
+
+                        
+                   <asp:ImageButton imageurl="images/save-file-option%20(1).png" runat="server" commandName="update" ToolTip="update" width="20px" Height="20px" />
+                   <asp:ImageButton imageurl="images/cancel%20(1).png" runat="server" commandName="cancel" ToolTip="cancel" width="20px" Height="20px" />
+                    </EditItemTemplate>
+                     <FooterTemplate>
+                         
+                          
+                           <asp:ImageButton imageurl="images/add-button%20(1).png" runat="server" commandName="add" ToolTip="add" width="20px" Height="20px" />
+                     </FooterTemplate>
+                </asp:TemplateField>
+            </Columns>
+            </asp:GridView>
+                <br />
+               
+        <asp:Label ID ="iblsuccessMessage" Text="" runat="server" ForeColor="Green" />
+        <br />
+        <asp:Label ID="iblErrorMessage" Text="" runat="server" ForeColor="Red" />
+             
+    </div>
+    </form>
+</body>
+</html>
---
+++ Edit.aspx.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+
+
+namespace WebInserteEdit
+{
+    public partial class Edit : System.Web.UI.Page
+    {
+        String connectionString = @"Data Source=DESKTOP-KR6N1AN;Initial Catalog=EditDb;Integrated Security=True";
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                PopulaterGridview();
+            }
+        }
+        void PopulaterGridview()
+        {
+            DataTable dtbl = new DataTable();
+            using (SqlConnection sqlcon = new SqlConnection(connectionString))
+            {
+                sqlcon.Open();
+                SqlDataAdapter sqlDa = new SqlDataAdapter("SELECT * FROM UserRegistration", sqlcon);
+                sqlDa.Fill(dtbl);
+
+                if (dtbl.Rows.Count > 0)
+                {
+                    gvUserRegistration.DataSource = dtbl;
+                    gvUserRegistration.DataBind();
+                }
+                else
+                {
+                    dtbl.Rows.Add(dtbl.NewRow());
+                    gvUserRegistration.DataSource = dtbl;
+                    gvUserRegistration.DataBind();
+                    gvUserRegistration.Rows[0].Cells.Clear();
+                    gvUserRegistration.Rows[0].Cells.Add(new TableCell());
+                    gvUserRegistration.Rows[0].Cells[0].ColumnSpan = dtbl.Columns.Count;
+                    gvUserRegistration.Rows[0].Cells[0].Text = "No Data Found ... ";
+                    gvUserRegistration.Rows[0].Cells[0].HorizontalAlign = HorizontalAlign.Center;
+                }
+            }
+        }
+
+        protected void gvUserRegistration_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+            try
+            {
+                if
+           (e.CommandName.Equals("add"))
+                {
+                    using (SqlConnection sqlcon = new SqlConnection(connectionString))
+                    {
+                        sqlcon.Open();
+                        string query = "INSERT INTO UserRegistration (firstname,lastname,contact,Email) VALUES(@firstname,@lastname,@contact,@Email)";
+                        SqlCommand sqlcmd = new SqlCommand(query, sqlcon);
+                        sqlcmd.Parameters.AddWithValue("@firstname", (gvUserRegistration.FooterRow.FindControl("txtfirstnamefooter") as TextBox).Text.Trim());
+
+                        sqlcmd.Parameters.AddWithValue("@lastname", (gvUserRegistration.FooterRow.FindControl("txtlastnamefooter") as TextBox).Text.Trim());
+
+                        sqlcmd.Parameters.AddWithValue("@contact", (gvUserRegistration.FooterRow.FindControl("txtcontactfooter") as TextBox).Text.Trim());
+                        sqlcmd.Parameters.AddWithValue("@Email", (gvUserRegistration.FooterRow.FindControl("txtEmailfooter") as TextBox).Text.Trim());
+                        sqlcmd.ExecuteNonQuery();
+                        PopulaterGridview();
+                        //iblsuccessMessage.Text = "NEW RECORD WAS ADDED ";
+                        Response.Write("<script>alert('REGISTRATION WAS SUCCESSFULL')</script>");
+                        iblErrorMessage.Text = "";
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                iblsuccessMessage.Text = "";
+                iblErrorMessage.Text = "ex.message";
+            }
+        }
+
+        protected void gvUserRegistration_RowEditing(object sender, GridViewEditEventArgs e)
+        {
+            gvUserRegistration.EditIndex = e.NewEditIndex;
+            PopulaterGridview();
+        }
+
+        protected void gvUserRegistration_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
+        {
+            gvUserRegistration.EditIndex = -1;
+            PopulaterGridview();
+        }
+
+        protected void gvUserRegistration_RowUpdating(object sender, GridViewUpdateEventArgs e)
+        {
+            try
+            {
+                    using (SqlConnection sqlcon = new SqlConnection(connectionString))
+                    {
+                        sqlcon.Open();
+                        string query = "UPDATE  UserRegistration SET firstname=@firstname,lastname=@lastname,contact=@contact,Email=@Email WHERE EditDb= @id";
+                       SqlCommand sqlcmd = new SqlCommand(query, sqlcon);
+                       sqlcmd.Parameters.AddWithValue("@firstname", (gvUserRegistration.Rows[e.RowIndex].FindControl("txtfirstname") as TextBox).Text.Trim());
+                       sqlcmd.Parameters.AddWithValue("@lastname", (gvUserRegistration.Rows[e.RowIndex].FindControl("txtlastname") as TextBox).Text.Trim());
+                       sqlcmd.Parameters.AddWithValue("@contact", (gvUserRegistration.Rows[e.RowIndex].FindControl("txtcontact") as TextBox).Text.Trim());
+                       sqlcmd.Parameters.AddWithValue("@Email", (gvUserRegistration.Rows[e.RowIndex].FindControl("txtEmail") as TextBox).Text.Trim());
+                       sqlcmd.Parameters.AddWithValue("@id", Convert.ToInt32(gvUserRegistration.DataKeys[e.RowIndex].Value.ToString()));
+                       sqlcmd.ExecuteNonQuery();
+                       gvUserRegistration.EditIndex = -1;
+                       PopulaterGridview();
+                       //iblsuccessMessage.Text = "selected Record update";
+                    Response.Write("<script>alert('REGISTRATION WAS UPDATE')</script>");
+                    iblErrorMessage.Text = "";
+                    }
+            }
+            catch (Exception ex)
+            {
+                iblsuccessMessage.Text = "";
+                iblErrorMessage.Text = "ex.message";
+            }
+        }
+
+        protected void gvUserRegistration_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            //try
+            //{
+                using (SqlConnection sqlcon = new SqlConnection(connectionString))
+                {
+                    sqlcon.Open();
+                    string query = "DELETE FROM UserRegistration WHERE EditDb = @id";
+                    SqlCommand sqlcmd = new SqlCommand(query, sqlcon);
+                    sqlcmd.Parameters.AddWithValue("@id", Convert.ToInt32(gvUserRegistration.DataKeys[e.RowIndex].Value.ToString()));
+                    sqlcmd.ExecuteNonQuery();
+                    PopulaterGridview();
+                    //iblsuccessMessage.Text = "selected Record Deleted";
+                    Response.Write("<script>alert('SELECTED RECORD DELETED')</script>");
+                    iblErrorMessage.Text = "";
+                }
+            //}
+            //catch (Exception ex)
+            //{
+            //    iblsuccessMessage.Text = "";
+            //    iblErrorMessage.Text = "ex.message";
+            //}
+        }
+    }
+}
